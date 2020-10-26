bb9fa0a87f6a5411a382d85a6dcfa5043c06c0ea
Roope Savolainen <roope.savolainen@gmail.com>
2020-10-26 15:53:58 +0200

    Added valid signalset to P300

--- AppServer/src/fi/aalto/app/server/AppNodeManager.java
+++ AppServer/src/fi/aalto/app/server/AppNodeManager.java
@@ -3,26 +3,27 @@ package fi.aalto.app.server;
 import java.io.File;
 import java.util.Locale;
 
-import com.prosysopc.ua.padim.PADIMType;
-import com.prosysopc.ua.padim.server.PADIMTypeNode;
+import com.prosysopc.ua.nodes.*;
+import com.prosysopc.ua.padim.*;
+import com.prosysopc.ua.padim.server.*;
+import com.prosysopc.ua.server.ServiceContext;
+import com.prosysopc.ua.server.nodes.*;
 import com.prosysopc.ua.stack.builtintypes.LocalizedText;
 import com.prosysopc.ua.stack.builtintypes.NodeId;
+import com.prosysopc.ua.stack.builtintypes.QualifiedName;
 import com.prosysopc.ua.stack.core.Identifiers;
 import com.prosysopc.ua.stack.core.BrowseDescription;
 import com.prosysopc.ua.stack.core.BrowseDirection;
 import com.prosysopc.ua.stack.core.BrowseResult;
 import com.prosysopc.ua.stack.core.ReferenceDescription;
-import com.prosysopc.ua.nodes.UaVariable; 
 import com.prosysopc.ua.StatusException;
-import com.prosysopc.ua.nodes.UaNode;
-import com.prosysopc.ua.nodes.UaNodeFactoryException;
 import com.prosysopc.ua.server.NodeManagerUaNode;
 import com.prosysopc.ua.server.UaInstantiationException;
 import com.prosysopc.ua.server.UaServer;
-import com.prosysopc.ua.server.nodes.PlainVariable;
-import com.prosysopc.ua.server.nodes.UaObjectNode;
 import com.prosysopc.ua.client.AddressSpace;
 import com.prosysopc.ua.client.UaClient;
+import com.prosysopc.ua.types.opcua.BaseDataVariableType;
+import com.prosysopc.ua.types.opcua.server.BaseDataVariableTypeNode;
 
 public class AppNodeManager extends NodeManagerUaNode {
 
@@ -42,6 +43,33 @@ public class AppNodeManager extends NodeManagerUaNode {
             final NodeId id = new NodeId(ns, "P300");
 			PADIMType P300 = createInstance(PADIMTypeNode.class, "P300", id);
 			objectsFolder.addComponent(P300);
+
+			final NodeId signalset_id = new NodeId(ns, "SignalSet");
+			SignalSetType P300_signalset = createInstance(SignalSetTypeNode.class, "SignalSet", signalset_id);
+			P300.addComponent(P300_signalset);
+
+			final NodeId measurement_id = new NodeId(ns, "Measurement");
+			AnalogSignalType P300_measurement = createInstance(AnalogSignalTypeNode.class, "Measurement", measurement_id);
+			P300_signalset.addComponent(P300_measurement);
+
+			UaNode P300_signal = P300_measurement.getComponent(new QualifiedName(2, "AnalogSignal"));
+
+			final NodeId simu_id = new NodeId(ns, "Simulation value");
+			PlainProperty<Number> simu = new PlainProperty<Number>(this, simu_id, "Simulation value", Locale.ENGLISH);
+			P300_signal.addComponent(simu);
+
+			final NodeId actual_id = new NodeId(ns, "Actual value");
+			PlainProperty<Number> actual = new PlainProperty<Number>(this, actual_id, "Actual value", Locale.ENGLISH);
+			P300_signal.addComponent(actual);
+
+			final NodeId state_id = new NodeId(ns, "Simulation state");
+			PlainProperty<Boolean> state = new PlainProperty<Boolean>(this, state_id, "Simulation state", Locale.ENGLISH);
+			P300_signal.addComponent(state);
+
+			final NodeId zeropointadjustment_id = new NodeId(ns, "ZeroPointAdjustment");
+			PlainMethod zpa = new PlainMethod(this, zeropointadjustment_id, "ZeroPointAdjustment", Locale.ENGLISH); //createInstance(PlainMethod.class, "ZeroPointAdjustment", zeropointadjustment_id);
+			P300_measurement.addComponent(zpa);
+
 		} catch (Exception e) {
 		    System.out.println(e.getMessage());
 	    }
